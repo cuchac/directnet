31e2fc647a7fb55985c6a446aecc83925c724cc5
joe <joe@wpj.cz>
2018-09-12 21:35:50 +0200

    Tag version 2.0

--- directnet/__init__.py
+++ directnet/__init__.py
@@ -1,6 +1,7 @@
-version = '1.2'
+version = '2.0'
 
 from .dn_client import DNClient
 from .ks_client import KSClient
+from .ks_net_client import KSNetClient
 
-__all__ = ('DNClient', 'KSClient')
+__all__ = ('DNClient', 'KSClient', 'KSNetClient')
